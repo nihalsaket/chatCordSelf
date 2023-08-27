e3e80fe6a1207c8fe67341947014b53af5c6a522
nihalsaket <nihalsaket@gmail.com>
2023-08-26 18:25:46 -0700

    Solved for username passed

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,19 @@
 const chatForm = document.getElementById('chat-form');
 
 const chatMessages = document.querySelector('.chat-messages');
+console.log("trying to bring username");
 
+// var username = "";
+// document.getElementById("submitbutton").addEventListener('click',function (e){
+//     username = document.getElementById("username").value;
+//     console.log("username fetched", username);
+// });
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const username = urlParams.get('username');
+
+console.log(username);
 
 const socket = io();
 
@@ -11,7 +23,6 @@ socket.on('message',message=>{
     outputMessage(message);
 
     //scroll down
-
     chatMessages.scrollTop=chatMessages.scrollHeight;
 
 });
@@ -33,13 +44,28 @@ chatForm.addEventListener('submit',(e)=>{
     e.target.elements.msg.focus();
 });
 
-//Render received message to the DOM
 
+
+//Render received message to the DOM
 function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML=`<p class="meta">Mary <span>9:15pm</span></p>
+    div.innerHTML=`<p class="meta">${username} <span>${getLocalTime()}</span></p>
 <p class="text">${message}</p>`;
 
     document.querySelector('.chat-messages').appendChild(div);
+}
+
+//function to pull username from form
+
+
+
+//function to update the username to the other person
+
+//function to get local time
+
+function getLocalTime() {
+    const now = new Date();
+    const options = { hour: 'numeric', minute: 'numeric'};
+    return now.toLocaleTimeString(undefined, options);
 }
